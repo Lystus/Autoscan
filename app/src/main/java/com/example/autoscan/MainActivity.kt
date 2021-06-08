2132ab7d949cd6292fd70de9510e9f14ba29feac
sharon <if19b170@technikum-wien.at>
2021-06-08 22:40:12 +0200

    basic piechart

--- app/src/main/java/com/example/autoscan/MainActivity.kt
+++ app/src/main/java/com/example/autoscan/MainActivity.kt
@@ -1,11 +1,59 @@
 package com.example.autoscan
 
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.Color
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.github.mikephil.charting.animation.Easing
+
+import com.github.mikephil.charting.animation.Easing.EaseInOutQuad
+import com.github.mikephil.charting.charts.PieChart
+import com.github.mikephil.charting.components.Legend
+import com.github.mikephil.charting.data.PieData
+import com.github.mikephil.charting.data.PieDataSet
+import com.github.mikephil.charting.data.PieEntry
+import com.github.mikephil.charting.formatter.PercentFormatter
+
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var pieChart: PieChart
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        pieChart = findViewById<PieChart>(R.id.pieChart)
+
+        pieChart.setUsePercentValues(true)
+        pieChart.setDrawEntryLabels(false)
+        pieChart.isRotationEnabled = false
+        pieChart.description.text = ""
+
+        pieChart.setUsePercentValues(true)
+        val dataEntries = ArrayList<PieEntry>()
+        dataEntries.add(PieEntry(73f, "Autoscan"))
+        dataEntries.add(PieEntry(27f, "Unfinished"))
+
+        val colors: ArrayList<Int> = ArrayList()
+        colors.add(Color.parseColor("#ED4A3E"))
+        colors.add(Color.parseColor("#CED4D6"))
+
+
+        val dataSet = PieDataSet(dataEntries, "")
+        val data = PieData(dataSet)
+
+        data.setValueFormatter(PercentFormatter())
+        dataSet.sliceSpace = 3f
+        dataSet.colors = colors
+        pieChart.data = data
+        pieChart.setCenterText("73%")
+        pieChart.setCenterTextSize(18F)
+        pieChart.animateY(1400, Easing.EaseInOutQuad)
+
+        pieChart.holeRadius = 80f
+        pieChart.transparentCircleRadius = 61f
+        pieChart.isDrawHoleEnabled = true
+        pieChart.setHoleColor(Color.WHITE)
+
+        pieChart.invalidate()
+
+
     }
 }
