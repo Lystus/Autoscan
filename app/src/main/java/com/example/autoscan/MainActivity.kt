dad69deac79a479910f0834082881025e9ffcb9e
sharon <if19b170@technikum-wien.at>
2021-06-20 16:56:48 +0200

    Viewpager added

--- app/src/main/java/com/example/autoscan/MainActivity.kt
+++ app/src/main/java/com/example/autoscan/MainActivity.kt
@@ -3,19 +3,25 @@ package com.example.autoscan
 import android.graphics.Color
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import androidx.viewpager2.widget.ViewPager2
 import com.github.mikephil.charting.animation.Easing
-
-import com.github.mikephil.charting.animation.Easing.EaseInOutQuad
 import com.github.mikephil.charting.charts.PieChart
 import com.github.mikephil.charting.components.Legend
 import com.github.mikephil.charting.data.PieData
 import com.github.mikephil.charting.data.PieDataSet
 import com.github.mikephil.charting.data.PieEntry
 import com.github.mikephil.charting.formatter.PercentFormatter
+private const val NUM_PAGES = 3
 
 
 class MainActivity : AppCompatActivity() {
     private lateinit var pieChart: PieChart
+
+    private lateinit var viewPager: ViewPager2
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -48,8 +54,8 @@ class MainActivity : AppCompatActivity() {
         dataSet.sliceSpace = 3f
         dataSet.colors = colors
         pieChart.data = data
-        pieChart.setCenterText("73%")
-        pieChart.setCenterTextSize(30F)
+        pieChart.setCenterText(" 73%")
+        pieChart.setCenterTextSize(35F)
         pieChart.animateY(1400, Easing.EaseInOutQuad)
 
         pieChart.holeRadius = 80f
@@ -64,14 +70,42 @@ class MainActivity : AppCompatActivity() {
         l.verticalAlignment = Legend.LegendVerticalAlignment.CENTER
         l.horizontalAlignment = Legend.LegendHorizontalAlignment.LEFT
         l.formSize = 5f
-        l.formToTextSpace = 0f
+        l.formToTextSpace = 5f
         l.form = Legend.LegendForm.SQUARE
-        l.textSize = 10f
+        l.textSize = 16f
         l.orientation = Legend.LegendOrientation.VERTICAL
         l.isWordWrapEnabled = true
 
         pieChart.invalidate()
+        viewPager = findViewById(R.id.pager)
+
+        // The pager adapter, which provides the pages to the view pager widget.
+        val pagerAdapter = ScreenSlidePagerAdapter(this)
+        viewPager.adapter = pagerAdapter
+
 
 
     }
+    override fun onBackPressed() {
+        if (viewPager.currentItem == 0) {
+            // If the user is currently looking at the first step, allow the system to handle the
+            // Back button. This calls finish() on this activity and pops the back stack.
+            super.onBackPressed()
+        } else {
+            // Otherwise, select the previous step.
+            viewPager.currentItem = viewPager.currentItem - 1
+        }
+    }
+
+    /**
+     * A simple pager adapter that represents 5 ScreenSlidePageFragment objects, in
+     * sequence.
+     */
+    private inner class ScreenSlidePagerAdapter(fa: FragmentActivity) : FragmentStateAdapter(fa) {
+        override fun getItemCount(): Int = NUM_PAGES
+
+        override fun createFragment(position: Int): Fragment = today()
+    }
+
+
 }
---
+++ app/src/main/java/com/example/autoscan/today.kt
@@ -0,0 +1,101 @@
+package com.example.autoscan
+
+import android.graphics.Color
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.github.mikephil.charting.animation.Easing
+import com.github.mikephil.charting.charts.PieChart
+import com.github.mikephil.charting.components.Legend
+import com.github.mikephil.charting.data.PieData
+import com.github.mikephil.charting.data.PieDataSet
+import com.github.mikephil.charting.data.PieEntry
+import com.github.mikephil.charting.formatter.PercentFormatter
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [today.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class today : Fragment() {
+    // TODO: Rename and change types of parameters
+    private var param1: String? = null
+    private var param2: String? = null
+   private lateinit var pie: PieChart
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+            param1 = it.getString(ARG_PARAM1)
+            param2 = it.getString(ARG_PARAM2)
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_today, container, false)
+        pie = view.findViewById<PieChart>(R.id.pie)
+        pie.setUsePercentValues(true)
+        pie.setDrawEntryLabels(false)
+        pie.setDrawMarkers(false)
+        pie.isRotationEnabled = false
+        pie.getDescription().setEnabled(false)
+        pie.setCenterText("50 min")
+        pie.setCenterTextColor(Color.WHITE)
+        pie.setCenterTextSize(15F)
+
+        pie.setUsePercentValues(false)
+        val dataEntries = ArrayList<PieEntry>()
+        dataEntries.add(PieEntry(100f, ""))
+
+
+        val colors: ArrayList<Int> = ArrayList()
+        colors.add(Color.parseColor("#ED4A3E"))
+
+
+        val dataSet = PieDataSet(dataEntries, "")
+        val data = PieData(dataSet)
+
+
+        dataSet.colors = colors
+        pie.data = data
+        pie.isDrawHoleEnabled = false
+        pie.legend.isEnabled = false
+
+        pie.invalidate()
+
+
+        // Inflate the layout for this fragment
+        return view
+    }
+
+    companion object {
+        /**
+         * Use this factory method to create a new instance of
+         * this fragment using the provided parameters.
+         *
+         * @param param1 Parameter 1.
+         * @param param2 Parameter 2.
+         * @return A new instance of fragment today.
+         */
+        // TODO: Rename and change types and number of parameters
+        @JvmStatic
+        fun newInstance(param1: String, param2: String) =
+            today().apply {
+                arguments = Bundle().apply {
+                    putString(ARG_PARAM1, param1)
+                    putString(ARG_PARAM2, param2)
+                }
+            }
+    }
+}
